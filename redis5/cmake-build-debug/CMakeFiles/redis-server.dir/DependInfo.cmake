8a70e4c072b6e31c2580127ee90bc29ea9342395
wangqy <wangqy@getui.com>
2019-09-24 23:26:18 +0800

    upload redis source code with cmake files

---
+++ redis5/cmake-build-debug/CMakeFiles/redis-server.dir/DependInfo.cmake
@@ -0,0 +1,96 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/u/forest/opensource/redis5/src/adlist.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/adlist.c.o"
+  "/Users/u/forest/opensource/redis5/src/ae.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/ae.c.o"
+  "/Users/u/forest/opensource/redis5/src/ae_kqueue.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/ae_kqueue.c.o"
+  "/Users/u/forest/opensource/redis5/src/anet.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/anet.c.o"
+  "/Users/u/forest/opensource/redis5/src/aof.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/aof.c.o"
+  "/Users/u/forest/opensource/redis5/src/bio.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/bio.c.o"
+  "/Users/u/forest/opensource/redis5/src/bitops.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/bitops.c.o"
+  "/Users/u/forest/opensource/redis5/src/blocked.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/blocked.c.o"
+  "/Users/u/forest/opensource/redis5/src/childinfo.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/childinfo.c.o"
+  "/Users/u/forest/opensource/redis5/src/cluster.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/cluster.c.o"
+  "/Users/u/forest/opensource/redis5/src/config.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/config.c.o"
+  "/Users/u/forest/opensource/redis5/src/crc16.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/crc16.c.o"
+  "/Users/u/forest/opensource/redis5/src/crc64.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/crc64.c.o"
+  "/Users/u/forest/opensource/redis5/src/db.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/db.c.o"
+  "/Users/u/forest/opensource/redis5/src/debug.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/debug.c.o"
+  "/Users/u/forest/opensource/redis5/src/defrag.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/defrag.c.o"
+  "/Users/u/forest/opensource/redis5/src/dict.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/dict.c.o"
+  "/Users/u/forest/opensource/redis5/src/endianconv.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/endianconv.c.o"
+  "/Users/u/forest/opensource/redis5/src/evict.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/evict.c.o"
+  "/Users/u/forest/opensource/redis5/src/expire.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/expire.c.o"
+  "/Users/u/forest/opensource/redis5/src/geo.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/geo.c.o"
+  "/Users/u/forest/opensource/redis5/src/geohash.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/geohash.c.o"
+  "/Users/u/forest/opensource/redis5/src/geohash_helper.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/geohash_helper.c.o"
+  "/Users/u/forest/opensource/redis5/src/hyperloglog.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/hyperloglog.c.o"
+  "/Users/u/forest/opensource/redis5/src/intset.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/intset.c.o"
+  "/Users/u/forest/opensource/redis5/src/latency.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/latency.c.o"
+  "/Users/u/forest/opensource/redis5/src/lazyfree.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/lazyfree.c.o"
+  "/Users/u/forest/opensource/redis5/src/listpack.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/listpack.c.o"
+  "/Users/u/forest/opensource/redis5/src/localtime.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/localtime.c.o"
+  "/Users/u/forest/opensource/redis5/src/lolwut.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/lolwut.c.o"
+  "/Users/u/forest/opensource/redis5/src/lolwut5.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/lolwut5.c.o"
+  "/Users/u/forest/opensource/redis5/src/lzf_c.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/lzf_c.c.o"
+  "/Users/u/forest/opensource/redis5/src/lzf_d.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/lzf_d.c.o"
+  "/Users/u/forest/opensource/redis5/src/memtest.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/memtest.c.o"
+  "/Users/u/forest/opensource/redis5/src/module.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/module.c.o"
+  "/Users/u/forest/opensource/redis5/src/multi.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/multi.c.o"
+  "/Users/u/forest/opensource/redis5/src/networking.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/networking.c.o"
+  "/Users/u/forest/opensource/redis5/src/notify.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/notify.c.o"
+  "/Users/u/forest/opensource/redis5/src/object.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/object.c.o"
+  "/Users/u/forest/opensource/redis5/src/pqsort.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/pqsort.c.o"
+  "/Users/u/forest/opensource/redis5/src/pubsub.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/pubsub.c.o"
+  "/Users/u/forest/opensource/redis5/src/quicklist.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/quicklist.c.o"
+  "/Users/u/forest/opensource/redis5/src/rand.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/rand.c.o"
+  "/Users/u/forest/opensource/redis5/src/rax.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/rax.c.o"
+  "/Users/u/forest/opensource/redis5/src/rdb.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/rdb.c.o"
+  "/Users/u/forest/opensource/redis5/src/redis-check-aof.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/redis-check-aof.c.o"
+  "/Users/u/forest/opensource/redis5/src/redis-check-rdb.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/redis-check-rdb.c.o"
+  "/Users/u/forest/opensource/redis5/src/release.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/release.c.o"
+  "/Users/u/forest/opensource/redis5/src/replication.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/replication.c.o"
+  "/Users/u/forest/opensource/redis5/src/rio.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/rio.c.o"
+  "/Users/u/forest/opensource/redis5/src/scripting.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/scripting.c.o"
+  "/Users/u/forest/opensource/redis5/src/sds.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/sds.c.o"
+  "/Users/u/forest/opensource/redis5/src/sentinel.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/sentinel.c.o"
+  "/Users/u/forest/opensource/redis5/src/server.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/server.c.o"
+  "/Users/u/forest/opensource/redis5/src/setproctitle.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/setproctitle.c.o"
+  "/Users/u/forest/opensource/redis5/src/sha1.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/sha1.c.o"
+  "/Users/u/forest/opensource/redis5/src/siphash.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/siphash.c.o"
+  "/Users/u/forest/opensource/redis5/src/slowlog.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/slowlog.c.o"
+  "/Users/u/forest/opensource/redis5/src/sort.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/sort.c.o"
+  "/Users/u/forest/opensource/redis5/src/sparkline.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/sparkline.c.o"
+  "/Users/u/forest/opensource/redis5/src/syncio.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/syncio.c.o"
+  "/Users/u/forest/opensource/redis5/src/t_hash.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/t_hash.c.o"
+  "/Users/u/forest/opensource/redis5/src/t_list.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/t_list.c.o"
+  "/Users/u/forest/opensource/redis5/src/t_set.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/t_set.c.o"
+  "/Users/u/forest/opensource/redis5/src/t_stream.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/t_stream.c.o"
+  "/Users/u/forest/opensource/redis5/src/t_string.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/t_string.c.o"
+  "/Users/u/forest/opensource/redis5/src/t_zset.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/t_zset.c.o"
+  "/Users/u/forest/opensource/redis5/src/util.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/util.c.o"
+  "/Users/u/forest/opensource/redis5/src/ziplist.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/ziplist.c.o"
+  "/Users/u/forest/opensource/redis5/src/zipmap.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/zipmap.c.o"
+  "/Users/u/forest/opensource/redis5/src/zmalloc.c" "/Users/u/forest/opensource/redis5/cmake-build-debug/CMakeFiles/redis-server.dir/src/zmalloc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../deps/linenoise"
+  "../deps/hiredis"
+  "../deps/lua/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/u/forest/opensource/redis5/cmake-build-debug/deps/lua/CMakeFiles/lua.dir/DependInfo.cmake"
+  "/Users/u/forest/opensource/redis5/cmake-build-debug/deps/linenoise/CMakeFiles/linenoise.dir/DependInfo.cmake"
+  "/Users/u/forest/opensource/redis5/cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ redis5/src/release.h
@@ -0,0 +1,3 @@
+#define REDIS_GIT_SHA1 "c1ccf0f1"
+#define REDIS_GIT_DIRTY "      14"
+#define REDIS_BUILD_ID "udeMacBook-Pro.local-1568553255"
